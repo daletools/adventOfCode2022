c955c5d0b5e19746e12fa6a569eb2b6c3bc4b355
daletools <dalerhendricks@outlook.com>
2022-12-07 12:44:25 -0800

    Set up day 7

---
+++ src/Day07.kt
@@ -0,0 +1,37 @@
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        var score = 0
+
+        return score
+    }
+
+    fun part2(input: List<String>): Int {
+        var score = 0
+
+        return score
+    }
+
+
+    val testInput = readInput("Day06_test")
+    println("Part 1 Test:")
+    println(part1(testInput))
+    println("Part 2 Test:")
+    println(part2(testInput))
+
+
+    val input = readInput("Day06")
+    println("Part 1:")
+    println(part1(input))
+    println("Part 2:")
+    println(part2(input))
+}
+
+fun parseLine(line: String) {
+    val commands = line.split(" ")
+}
+
+class Dir ()
+
+class File()
+
